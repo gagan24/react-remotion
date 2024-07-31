749beb66b01a3424c547b68a96d756b292e11b93
= <gagandeep.zversal@gmail.com>
2024-07-31 13:34:10 +0530

    removed unwanted code

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,20 +1,4 @@
-// import MyComposition from "./Composition";
-// export const RemotionRoot: React.FC = () => {
-//   return (
-//     <>
-//       <Composition
-//         id="MyComp"
-//         component={MyComposition}
-//         durationInFrames={120}
-//         fps={30}
-//         width={1280}
-//         height={720}
-//       />
-//     </>
-//   );
-// };
-
-import { Composition } from "remotion";
+// import { Composition } from "remotion";
 
 import React, { useState } from "react";
 import VideoPlayer from "./components/VideoPlayer";
